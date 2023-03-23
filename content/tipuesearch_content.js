d0b70ac763072f8a2d74e7eb8b1234cade07c9af
tseYU000 <41023206@gm.nfu.edu.tw>
2023-03-23 16:09:27 +0800

    2

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2b-pj1bg9 \n 網站:2 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2b-pj1bg9 \n \n 網站:2 \n \n', 'tags': '', 'url': 'About.html'}]};
